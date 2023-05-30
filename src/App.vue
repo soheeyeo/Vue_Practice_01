989c615af128abef0dbe762d64da6353f67ee748
soheeyeo <sohee407@gmail.com>
2023-05-30 23:56:15 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <img alt="Vue logo" src="./assets/logo.png">
+        <div>
+            <h4>{{ products[0] }}</h4>
+            <p>70 만원</p>
+        </div>
+        <div>
+            <h4>{{ products[1] }}</h4>
+            <p>50 만원</p>
+        </div>
+                <div>
+            <h4>{{ products[2] }}</h4>
+            <p>60 만원</p>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data(){
+        return {
+            products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+        }
+    },
+    components: {
+    
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
