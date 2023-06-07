96050e16d4b265286cb2ef1b05a0c737ca915e82
soheeyeo <sohee407@gmail.com>
2023-06-07 20:15:22 +0900

    이벤트 03

--- src/App.vue
+++ src/App.vue
@@ -17,15 +17,17 @@
         <div>
             <h4>{{ products[0] }}</h4>
             <p>50만원</p>
-            <button v-on:click="increase">허위매물신고</button> <span>신고수: {{ 신고수 }}</span>
+            <button v-on:click="신고수[0]++">허위매물신고</button> <span>신고수: {{ 신고수[0] }}</span>
         </div>
-                <div>
+        <div>
             <h4>{{ products[1] }}</h4>
             <p>60만원</p>
+            <button v-on:click="신고수[1]++">허위매물신고</button> <span>신고수: {{ 신고수[1] }}</span>
         </div>
-                <div>
+        <div>
             <h4>{{ products[2] }}</h4>
             <p>70만원</p>
+            <button v-on:click="신고수[2]++">허위매물신고</button> <span>신고수: {{ 신고수[2] }}</span>
         </div>
     </div>
 </template>
@@ -36,7 +38,7 @@ export default {
     name: 'App',
     data(){
         return {
-            신고수 : 0,
+            신고수 : [0, 0, 0],
             메뉴들 : ['Home', 'Shop', 'About'],
             products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
         }
