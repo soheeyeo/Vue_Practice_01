de94a968e0923051b5d1b3316c3e7cc36697706d
soheeyeo <sohee407@gmail.com>
2023-06-09 10:15:22 +0900

    v-if 01

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
+        <div class="black-bg" v-if="모달창열렸니 == true">
+            <div class="white-bg">
+                <h4>상세페이지임</h4>
+                <p>상세페이지 내용임</p>
+            </div>
+        </div>
+
         <div class="menu">
             <a v-for="i in 메뉴들" :key="i">{{ i }}</a>
         </div>
@@ -16,7 +23,7 @@
 
         <div>
             <img src="./assets/room0.jpg" class="room-img">
-            <h4>{{ products[0] }}</h4>
+            <h4 @click="모달창열렸니 = true">{{ products[0] }}</h4>
             <p>50만원</p>
             <button v-on:click="신고수[0]++">허위매물신고</button> <span>신고수: {{ 신고수[0] }}</span>
         </div>
@@ -41,6 +48,7 @@ export default {
     name: 'App',
     data(){
         return {
+            모달창열렸니 : true,
             신고수 : [0, 0, 0],
             메뉴들 : ['Home', 'Shop', 'About'],
             products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
@@ -58,6 +66,29 @@ export default {
 </script>
 
 <style>
+body {
+    margin: 0;
+}
+
+div {
+    box-sizing: border-box;
+}
+
+.black-bg {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    padding: 20px;
+}
+
+.white-bg {
+    width: 100%;
+    background: white;
+    border-radius: 8px;
+    padding: 20px;
+}
+
 .room-img {
     width: 100%;
     margin-top: 40px;
