7cc126d606f10f0a8a3768f9bc88387c9b45e4d1
soheeyeo <sohee407@gmail.com>
2023-06-14 10:59:02 +0900

    모달창 상세페이지 02

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
         <div class="black-bg" v-if="모달창열렸니 == true">
             <div class="white-bg">
                 <h4>{{ 원룸들[누른거].title }}</h4>
-                <p>상세페이지 내용임</p>
+                <img :src="원룸들[누른거].image" class="room-img">
+                <p>{{ 원룸들[누른거].price }}</p>
+                <p>{{ 원룸들[누른거].content }}</p>
                 <button v-on:click="모달창열렸니 = false">닫기</button>
             </div>
         </div>
