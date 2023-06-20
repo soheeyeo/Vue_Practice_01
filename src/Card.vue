a2b541a44fa94e0ec71b8ad0a709dd0a4beb9cac
soheeyeo <sohee407@gmail.com>
2023-06-20 11:30:01 +0900

    custom event 02

--- src/Card.vue
+++ src/Card.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <img :src="원룸.image" class="room-img">
-        <h4 @click="$emit('openModal', 원룸.id)">{{ 원룸.title }}</h4>
+        <h4 @click="send">{{ 원룸.title }}</h4>
         <p>{{ 원룸.price }}원</p>
     </div>
 </template>
@@ -11,6 +11,11 @@ export default {
     name : 'CardItem',
     props : {
         원룸 : Object,
+    },
+    methods : {
+        send() {
+            this.$emit('openModal', this.원룸.id)
+        }
     }
 }
 </script>
