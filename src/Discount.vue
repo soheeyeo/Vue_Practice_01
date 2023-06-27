7997cd644adc4f94f5cbf8cd8502b53fc851489c
soheeyeo <sohee407@gmail.com>
2023-06-28 00:03:09 +0900

    lifecycle 02

--- src/Discount.vue
+++ src/Discount.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="discount">
-        <h4>지금 결제하면 20% 할인</h4>
+        <h4>지금 결제하면 {{할인율}}% 할인</h4>
     </div>
 </template>
 
 <script>
 export default {
     name: 'DiscountBanner',
+    props : {
+        할인율 : Number,
+    }
 }
 </script>
 
