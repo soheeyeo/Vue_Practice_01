67a4aeba6ac1258220c3f1a2faf4d79c45dbfc81
soheeyeo <sohee407@gmail.com>
2023-06-28 00:06:17 +0900

    lifecycle 03

--- src/Modal.vue
+++ src/Modal.vue
@@ -28,6 +28,12 @@ export default {
             }
         },
     },
+    beforeUpdate(){
+        if(this.month == 2){
+            alert('3개월 이상 가능합니다.')
+            this.month = 3;
+        }
+    },
     props : {
         원룸들 : Array,
         누른거 : Number,
